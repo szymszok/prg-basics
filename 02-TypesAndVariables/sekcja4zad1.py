da3ab333c9d3de97e68ef01cd9de50a61f8d5e15
Szymon Marszalek <szymon.prezentacje@wp.pl>
2024-10-08 14:22:26 +0200

    sekcja4zrobione

---
+++ 02-TypesAndVariables/sekcja4zad1.py
@@ -0,0 +1,6 @@
+name = "Adam"
+age = 20
+height = 170
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age + 6} years old.")
