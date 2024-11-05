57dff66d1d308562ddfc6d0dcfe0c42bdfab9ae4
Szymon <szymon.prezentacje@wp.pl>
2024-11-05 11:22:34 +0100

    poprawka

--- 04-Functions/7.19.py
+++ 04-Functions/7.19.py
@@ -2,10 +2,11 @@ def summary(number):
     full_sum = 0
     digit = 0
     for char in number:
-        char = int(digit)
-        if digit == digit:
-            full_sum += digit
-    return full_sum
+      if number.count(char)  > 1:
+        full_sum += int(number)
 
-number = input('Enter numbers: ')
-print(summary(number))
+    return full_sum
+        
+
+
+print(summary('230335'))#nie dziala 
